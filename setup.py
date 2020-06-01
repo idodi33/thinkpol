fd40a81dfe52b5625d4693085ab54bcf09fa3662
Ido Bar-Sella <idodi33@gmail.com>
2020-06-01 11:59:30 +0300

    updated requirements and setup

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'thinkpol',
-    version = '0.1.0',
+    version = '1.0.0',
     author = 'Ido Bar-Sella',
     description = 'My final project in the course \'Advanced System Design\'.',
     packages = find_packages(),
-    install_requires = ['click', 'flask'],
+    install_requires = ['click', 'flask', 'furl', 'pymongo', 'flask_cors', 'requests',
+    'werkzeug.routing', 'bson.json_util', 'pika', 'PIL', 'matplotlib.pyplot', 'numpy'],
     tests_require = ['pytest', 'pytest-cov'],
 )
